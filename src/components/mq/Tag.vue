0b02da30a3017a33afc0fa9a357a38b852a5fbbf
mq1 <chan-mai@mq1.dev>
2025-04-25 02:32:09 +0900

    style: タグの挙動を変更

--- src/components/mq/Tag.vue
+++ src/components/mq/Tag.vue
@@ -11,10 +11,10 @@ const props = withDefaults(defineProps<{
 });
 </script>
 <template>
-    <a v-if="tag" :href="`/tag/${tag.id}`" class="inline-flex items-center px-3 py-1.5 rounded-full text-sm 
+    <a v-if="tag" :href="`/tag/${tag.id}`" class="group inline-flex items-center px-3 py-1.5 rounded-full text-sm 
             bg-white border border-primary text-gray-700 hover:bg-primary hover:text-white
             transition-colors duration-200 shadow-sm">
-        <Icon v-if="!tag.count" name="material-symbols:tag-rounded" class="mr-0.5 size-5 text-primary" />
+        <Icon v-if="!tag.count" name="material-symbols:tag-rounded" class="mr-0.5 size-5 text-primary group-hover:text-white" />
         <span class="mr-1.5">{{ tag.name }}</span>
         <span v-if="tag.count" class="text-xs bg-primary text-white rounded-full px-1.5 py-0.5">{{ tag.count }}</span>
     </a>

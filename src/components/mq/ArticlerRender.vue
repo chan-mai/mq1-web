d45a1bd6f1e7a463c4be778fc975ac355bbb94a7
mq1 <chan-mai@mq1.dev>
2025-07-30 00:40:02 +0900

    style: シャドーいらないかも

--- src/components/mq/ArticlerRender.vue
+++ src/components/mq/ArticlerRender.vue
@@ -226,7 +226,6 @@ onMounted(() => {
     overflow-x: auto;
     max-width: 100%;
     border: 1px solid #e5e7eb;
-    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 }
 
 .micro-cms li {

72dace39c852d0fec12e9678ef7dd297531d2489
mq1 <chan-mai@mq1.dev>
2025-08-23 02:39:45 +0900

    chore: workersやめる

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,4 +1,6 @@
 <template>
+    <SpeedInsights/>
+    <Analytics/>
     <main class="bg-back text-slate-600">
         <SiteHeader />
         <div class="relative z-0 mx-auto min-w-screen box-content flex min-h-[calc(100vh-30rem)] flex-col px-6">

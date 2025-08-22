fa421cd91c663bacf3409806853996127b737b72
mq1 <chan-mai@mq1.dev>
2025-08-23 02:55:06 +0900

    chore

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+import { Analytics } from '@vercel/analytics/nuxt'
+</script>
 <template>
     <SpeedInsights/>
     <Analytics/>

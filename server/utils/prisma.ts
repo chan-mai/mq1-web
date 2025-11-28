394d69fe7c3316f646a1a558993af09b60a91214
mq1 <chan-mai@mq1.dev>
2025-11-28 19:32:50 +0000

    upgrade: several packages to the latest

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -1,14 +1,22 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../generated/prisma/client";
+import pg from "pg";
 
-// Prismaクライアントのシングルトンインスタンス
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
+const { Pool } = pg;
 
-export const prisma = global.prisma || new PrismaClient();
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
 
 if (process.env.NODE_ENV !== "production") {
-  global.prisma = prisma;
+  globalForPrisma.prisma = prisma;
 }
-

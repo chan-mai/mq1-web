fd49587804aaa08112e2838c115b2d75c024ed6f
mq1 <chan-mai@mq1.dev>
2025-12-09 04:28:17 +0000

    enhance: canonicalの追加

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,28 @@
+<script setup lang="ts">
+const runtimeConfig = useRuntimeConfig()
+const route = useRoute()
+
+useHead(() => {
+  const siteUrl = runtimeConfig.public.siteUrl.replace(/\/$/, '')
+  const path = route.path
+  
+  const url = new URL(path, siteUrl)
+  
+  if (route.query.page) {
+    url.searchParams.set('page', route.query.page.toString())
+  }
+  
+  return {
+    link: [
+      {
+        rel: 'canonical',
+        href: url.toString()
+      }
+    ]
+  }
+})
+</script>
+
 <template>
   <NuxtRouteAnnouncer />
   <NuxtLayout>
